ca4e250a2ef443336c514f8d196aa16e77999284
Waweru <ngashalex8@gmail.com>
2023-07-31 16:47:57 +0300

    Dev branch

---
+++ .github/workflows/person.rb
@@ -0,0 +1,32 @@
+class Person
+    attr_reader :id, :name, :age
+    def initialize (name = "Unknown", age =nil, parent_permission = true)
+        @id = generate_id
+        @age = age
+        @name = name
+        @parent_permission = parent_permission
+    end
+
+    def age =(age)
+        @age = age
+    end 
+
+    def name =(name)
+        @name = name
+    end 
+
+    def can_use_services?
+        of_age? || @parent_permission
+    end
+
+    private
+
+    def of_age?
+        @age.to_i >= 18
+    end
+
+    def generate_id 
+        rand(1..1000)
+    end
+
+end
