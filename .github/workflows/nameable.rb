07fddfa2fcfcc60f401007ca08001b2faa0ec2a5
Waweru <ngashalex8@gmail.com>
2023-08-01 14:02:44 +0300

    Created a file for basedecorator

--- .github/workflows/nameable.rb
+++ .github/workflows/nameable.rb
@@ -4,29 +4,4 @@ class Nameable
     end
   end
   
-  class BaseDecorator < Nameable
-    def initialize(nameable)
-      @nameable = nameable
-    end
-  
-    def correct_name
-      @nameable.correct_name
-    end
-  end
-  
-
-  class CapitalizeDecorator < BaseDecorator
-    def correct_name
-      original_name = @nameable.correct_name
-      original_name.capitalize
-    end
-  end
-  
-  class TrimmerDecorator < BaseDecorator
-    def correct_name
-      original_name = @nameable.correct_name
-      trimmed_name = original_name[0..9] 
-      trimmed_name
-    end
-  end
   
--- .github/workflows/person.rb
+++ .github/workflows/person.rb
@@ -1,4 +1,6 @@
 require_relative 'Nameable'
+require_relative 'BaseDecorator'
+
 
 class Person < Nameable
     attr_accessor :name, :age
