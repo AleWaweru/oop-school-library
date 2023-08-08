3f84b408f5345ab0b5475e1014f514abecdd48be
AleWaweru <ngashalex8@gmail.com>
2023-08-08 12:09:16 +0300

    Removed line 8 main.rb file

--- main.rb
+++ main.rb
@@ -5,12 +5,7 @@ require_relative 'handle_option'
 def main
   app = App.new
   option = 0
-
-  until option == 7
-    Menu.display_menu
-    option = Menu.options
-    Option.handle_option(option, app)
-  end
+  
 end
 
 main
